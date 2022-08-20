a9012b02ff3af5671ffd2f4d6702558093ee9958
Abraham E. Tavarez <abetavarez@icloud.com>
2022-08-19 23:09:13 -0400

    constants for emojis and keyboard keys

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+export const colors = {
+  black: '100F0F',
+  darkgrey: '413F42',
+  grey: '7F8487',
+  lightgrey: 'F1EEE9',
+  primary: '59CE8F',
+  secondary: 'FFEE63'
+};
+
+export const colorsToEmoji = {
+  [colors.darkgrey]: '🔴',
+  [colors.primary]: '🟢',
+  [colors.secondary]: '🟡'
+};
+
+export const ENTER = 'ENTER';
+export const CLEAR = 'CLEAR';
+
+export const keys = [
+  ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+  ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+  [ENTER, 'z', 'x', 'c', 'v', 'b', 'n', 'm', CLEAR]
+];
