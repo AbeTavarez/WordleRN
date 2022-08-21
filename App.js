c52fd2522cd64fc7163cfce048865b6602149fab
Abraham E. Tavarez <abetavarez@icloud.com>
2022-08-20 22:14:32 -0400

    game grid created

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { colors } from './src/constants';
+import Keyboard from './src/Keyboard/Keyboard';
+
+const NUMBER_OF_TRIES = 6;
 
 export default function App() {
+  const word = 'hello';
+  const letters = word.split('');
+
+  const rows = new Array(NUMBER_OF_TRIES).fill(
+    new Array(letters.length).fill('a')
+  );
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="light" />
       <Text style={styles.title}>Wordle</Text>
+
+      <View style={styles.map}>
+        {rows.map((row) => (
+          <View style={styles.row}>
+            {row.map((cell) => (
+              <View style={styles.cell}>
+                <Text style={styles.cellText}>{cell.toUpperCase()}</Text>
+              </View>
+            ))}
+          </View>
+        ))}
+      </View>
+
+      <Keyboard />
     </SafeAreaView>
   );
 }
@@ -15,7 +39,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.black,
-    // backgroundColor: '#884',
     alignItems: 'center'
     // justifyContent: 'center'
   },
@@ -24,5 +47,30 @@ const styles = StyleSheet.create({
     fontSize: 32,
     fontWeight: 'bold',
     letterSpacing: 10
+  },
+  map: {
+    alignSelf: 'stretch',
+    height: 100,
+    marginTop: 20
+  },
+  row: {
+    alignSelf: 'stretch',
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  cell: {
+    borderColor: colors.grey,
+    borderWidth: 3,
+    flex: 1,
+    aspectRatio: 1,
+    margin: 3,
+    maxWith: 70,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  cellText: {
+    color: colors.grey,
+    fontWeight: 'bold',
+    fontSize: 28
   }
 });
