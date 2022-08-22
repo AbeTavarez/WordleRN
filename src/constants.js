2279583e4ee69247be4a436f8a0839fd0ed47fbc
Abraham E. Tavarez <abetavarez@icloud.com>
2022-08-21 21:09:48 -0400

    comments and notes

--- src/constants.js
+++ src/constants.js
@@ -7,15 +7,19 @@ export const colors = {
   secondary: '#FFEE63'
 };
 
+//object mapping color from colors object to emoji
 export const colorsToEmoji = {
   [colors.darkgrey]: '🔴',
   [colors.primary]: '🟢',
   [colors.secondary]: '🟡'
 };
 
+// ENTER and CLEAR are 'long buttons'
 export const ENTER = 'ENTER';
 export const CLEAR = 'CLEAR';
 
+// keys array to represent the keyboard values
+// each nested array is a 'keyRow' of the keyboard
 export const keys = [
   ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
   ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
