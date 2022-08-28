156942a1a68174c4c2227ab3f462441ae594dff6
Abraham E. Tavarez <abetavarez@icloud.com>
2022-08-27 22:53:32 -0400

    clearn and enter keys swapped

--- src/constants.js
+++ src/constants.js
@@ -23,5 +23,5 @@ export const CLEAR = 'CLEAR';
 export const keys = [
   ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
   ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
-  [ENTER, 'z', 'x', 'c', 'v', 'b', 'n', 'm', CLEAR]
+  [CLEAR, 'z', 'x', 'c', 'v', 'b', 'n', 'm', ENTER]
 ];
